52fac7855d87cf1a233914b5e8ccc4ca91af4ece
Brandon Fraune <brandon.fraune@gmail.com>
2022-09-04 20:51:03 -0500

    trying to simplify some logic and make the temperature thing work better

---
+++ BLEPeripheral/Gatt/Temperature.swift
@@ -0,0 +1,16 @@
+//
+//  Temperature.swift
+//  BLEPeripheral
+//
+//  Created by Brandon Fraune on 9/4/22.
+//
+
+import Foundation
+
+class Temperature {
+    // let value = "06760100FFDB07081612310001" // 98.1 ºF
+    // let value = "06710100FFDB070816151E0001" // 98.4 ºF
+    // let value = "066A0100FFDB07081615220001" // 97.2 ºF
+    
+    let value = "06710100FFDB070816151E0001" // 98.4 ºF
+}
--- BLEPeripheral/Main/BluetoothViewModel.swift
+++ BLEPeripheral/Main/BluetoothViewModel.swift
@@ -17,8 +17,12 @@ class BluetoothViewModel: NSObject, ObservableObject, CBPeripheralManagerDelegat
     @Published var readValue = "98.1 ºF"
     @Published var writeValue = ""
     
-    private var service: CBUUID!
-    private let value = "06760100FFDB07081612310001" // 98.1 ºF
+    // Health Thermometer GATT service
+    private let service: CBUUID = CBUUID(string: "1809")
+    // Temperature Measurement GATT Characteristic
+    private let characteristicA = CBMutableCharacteristic(type: CBUUID(string: "2A1C"), properties: [.notify], value: nil, permissions: [.readable])
+    // private let characteristicB = CBMutableCharacteristic(type: CBUUID(string: "2A1C"), properties: [.read], value: valueData, permissions: [.readable])
+    // private let characteristicC = CBMutableCharacteristic(type: CBUUID(string: "2A1C"), properties: [.notify, .write, .read], value: nil, permissions: [.readable, .writeable])
     
     override init() {
         super.init()
@@ -47,53 +51,29 @@ class BluetoothViewModel: NSObject, ObservableObject, CBPeripheralManagerDelegat
     }
     
     private func addServices() {
-        let valueData = value.hexadecimal
-        
-        // 1. Create instance of CBMutableCharcateristic // Temperature Measurement GATT Characteristic
-        // let characteristic1 = CBMutableCharacteristic(type: CBUUID(string: "2A1C"), properties: [.read], value: valueData, permissions: [.readable]) // simple read
-        let characteristic2 = CBMutableCharacteristic(type: CBUUID(string: "2A1C"), properties: [.notify], value: nil, permissions: [.readable]) // notify only
-        // let characteristic3 = CBMutableCharacteristic(type: CBUUID(string: "2A1C"), properties: [.notify, .write, .read], value: nil, permissions: [.readable, .writeable])
-        
-        // 2. Create instance of CBMutableService // Health Thermometer GATT service
-        let service = CBMutableService(type: CBUUID(string: "1809"), primary: true)
-        
-        // 3. Add characteristics to the service
-        service.characteristics = [characteristic2]
-        
-        // 4. Add service to peripheralManager
-        peripheralManager.add(service)
-        
-        // 5. Start advertising
+        let myService = CBMutableService(type: service, primary: true)
+        myService.characteristics = [characteristicA]
+        peripheralManager.add(myService)
         startAdvertising()
     }
     
     private func startAdvertising() {
-        message = "Advertising Data"
-        peripheralManager.startAdvertising([CBAdvertisementDataLocalNameKey : "TAIDOC TD1242-BJF", CBAdvertisementDataServiceUUIDsKey : [service]])
-        print("Started Advertising")
+        message = "Advertising Data Started"
+        peripheralManager.startAdvertising([CBAdvertisementDataLocalNameKey: "TAIDOC TD1242-BJF", CBAdvertisementDataServiceUUIDsKey: [service]])
     }
     
     func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveRead request: CBATTRequest) {
-        message = "Data getting Read"
-//        readValue = value
-        
-        // Perform your additional operations here
-        
+        message = "Characteristic read"
     }
     
     func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveWrite requests: [CBATTRequest]) {
-        
-        message = "Writing Data"
-        
-//        if let value = requests.first?.value {
-//            writeValueLabel.text = value.hexEncodedString()
-//            // Perform here your additional operations on the data you get
-//        }
+        message = "Received write"
     }
     
     func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didSubscribeTo characteristic: CBCharacteristic) {
-        print("got subscribed to")
-        peripheral.updateValue(value.data(using: .utf8)!, for: myCharacteristic1, onSubscribedCentrals: [central])
+        message = "Subscriber added"
+        let value = Temperature().value
+        peripheral.updateValue(value.hexadecimal!, for: characteristicA, onSubscribedCentrals: [central])
     }
-
+    
 }
